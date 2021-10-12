00fb637a1cf8893fd0016c6714a39fa34e7713b3
trm621 <t.maher621@gmail.com>
2021-10-12 15:34:06 -0400

    added logic to button with windows object

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,66 @@
 // Assignment code here
 
+// Random number select
+var randomNum = Math.floor(Math.random() * 9);
+  console.log(randomNum);
+
+// Select the length of the password
+
+var promptLength = function() {
+  var charNum = window.prompt("How many characters would you like your password to be?")  
+    if (charNum < 8 || charNum > 128) {
+      window.alert("Your password must be at least 8 characters and no more than 128 characters. Please pick a valid option!")
+    return promptLength();
+    }
+    else { (console.log(charNum));
+    }
+};
+
+// Select whether to use special characters or not
+
+var selectCharacterTypes = function() {
+  confirmSpecial = window.confirm("Would you like to include special characters in your password?")
+    if (confirmSpecial === true) {
+      console.log(true);
+      }
+    else {
+      console.log(false);
+    }
+  confirmLowerCase = window.confirm("Would you like to include lowercase characters in your password?")
+    if (confirmLowerCase === true) {
+      console.log(true);
+      }
+      else {
+      console.log(false);
+    }
+  confirmUpperCase = window.confirm("Would you like to include uppercase characters in your password?")
+    if (confirmUpperCase === true) {
+      console.log(true);
+      } 
+      else {
+      console.log(false);
+    }
+  confirmNumericChars = window.confirm("Would you like to include numeric characters in your password?")
+    if (confirmNumericChars === true) {
+      console.log(true);
+      }
+      else {
+      console.log(false);
+    }
+  };
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
+  promptLength();
+  selectCharacterTypes();
 
-  passwordText.value = password;
-
-}
+  //passwordText.value = password;
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
