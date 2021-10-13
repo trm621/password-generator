26ddc7fa95e3f9ed311ae904839c47feef92d718
trm621 <t.maher621@gmail.com>
2021-10-13 12:02:52 -0400

    added uppercase, lowercase, and special character arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,11 @@
-// Assignment code here
+// Character arrays
+
+const upperCaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+"K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const lowerCaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+"l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const specialChars = ["!", "#", "$", "%", "&", "(", ")", "*", "-", "+", ":", ";",
+"<", ">", "?", "@", "[", "]", "/", "^", "_", "{", "}", "|", "~", "`", "=", ".", ","];
 
 // Random number select
 var randomNum = Math.floor(Math.random() * 9);
